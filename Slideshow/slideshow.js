ec5dcdc6f07cfd3f60bf865e5803b2b2bebe8027
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-04-17 15:18:35 -0700

    Working on carousel

--- Slideshow/slideshow.js
+++ Slideshow/slideshow.js
@@ -1,50 +1,49 @@
-class DriverCarousel {
+class Carousel {
     constructor(element){
         this.element = element;
-        console.log(this.element); 
+        // console.log(this.element); 
         this.leftButton = element.querySelector('.left-button');
         this.rightButton = element.querySelector('.right-button');
-        this.data = this.element.dataset.img; 
-        console.log(this.data); 
+        this.images = element.querySelectorAll('img');
+        
+        // console.log(this.images); 
+        // this.DriverImages = this.images.dataset.img; 
+        // this.data = this.element.dataset.img; 
+        // console.log(this.data); 
 
         // console.log(this.image); 
         // this.data = this.images.dataset.img;
         // console.log(this.data); 
 
 
-        // this.driverImages = this.images.dataset.img;
-        // console.log(driverImages);
-
-
-        // console.log(this.images);
-        // this.driverImages = images.dataset.driver; 
-        // this.currentIndex = 0;
-        // this.driverImages[0].style.display = 'block';
-        this.leftButton.addEventListener('click', () => {this.leftButtonClick()})
-        this.rightButton.addEventListener('click', () => { this.rightButtonClick() })
+        
+         this.currentIndex = 0;
+         this.images[0].style.display = 'block';
+         this.leftButton.addEventListener('click', () => {this.leftButtonClick()})
+         this.rightButton.addEventListener('click', () => { this.rightButtonClick() })
     }
 
     leftButtonClick(){
-        this.driverImages[this.currentIndex].style.display = 'none'
+        this.images[this.currentIndex].style.display = 'none'
         if(this.currentIndex === 0){
-            this.currentIndex = this.driverImages.length - 1
+            this.currentIndex = this.images.length - 1
         } else {
             this.currentIndex = this.currentIndex - 1
         }
-        this.driverImages[this.currentIndex].style.display = 'block'
+        this.images[this.currentIndex].style.display = 'block'
     }
 
     rightButtonClick(){
-        this.driverImages[this.currentIndex].style.display = 'none'
-        if (this.currentIndex === this.driverImages.length - 1) {
+        this.images[this.currentIndex].style.display = 'none'
+        if (this.currentIndex === this.images.length - 1) {
             this.currentIndex = 0
         } else {
             this.currentIndex = this.currentIndex + 1
         }
-        this.driverImages[this.currentIndex].style.display = 'block'
+        this.images[this.currentIndex].style.display = 'block'
     }
 }
 
-let driverCarousel = document.querySelector('.driver-carousel');
-driverCarousel = new DriverCarousel(driverCarousel);
-// console.log(driverCarousel);
+let carousel = document.querySelector('.carousel');
+carousel = new Carousel(carousel);
+//  console.log(Carousel);
