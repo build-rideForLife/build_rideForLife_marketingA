59cdbea40325e14da3db9f5aa1168f8cab6ba063
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-04-16 22:37:05 -0700

    Adding Google font

--- components/dropdown/dropdown.js
+++ components/dropdown/dropdown.js
@@ -6,7 +6,7 @@ class Dropdown {
       this.dropdown = dropdown;
       
       // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-      this.button = this.dropdown.querySelector('.dropdown-button');
+      this.button = this.dropdown.querySelector('.menu-button');
       
       // Assign the reference to the ".dropdown-content" class found in the dropdown element
       this.content = this.dropdown.querySelector('.dropdown-content');
