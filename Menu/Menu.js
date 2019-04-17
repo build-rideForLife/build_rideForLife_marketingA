9c2dfaa10865370ff212b048855bfda0999ed586
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-04-17 09:20:49 -0700

    Dropdown hamburger menu functionality

---
+++ Menu/Menu.js
@@ -0,0 +1,16 @@
+// Reference to the ".menu" class
+const menu = document.querySelector('.menu'); 
+//console.log(menu); 
+
+// Setting up my toggleMenu function to toggle the "menu--open" class on my menu reference. 
+const toggleMenu = () => { 
+    // menu.classList.toggle('menu'); 
+    menu.classList.toggle('menu--open'); 
+  }
+
+  // Reference to the ".menu-button" class
+   const menuButton = document.querySelector('.menu-button');
+   //console.log(menuButton)
+  
+  // Adding event listener to menuButton to callback toggleMenu on click 
+  menuButton.addEventListener('click', toggleMenu);
