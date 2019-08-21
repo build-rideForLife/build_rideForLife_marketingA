debab574891710ea3ac4d8118d689669e6b0d88f
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-08-21 10:17:21 -0500

    Added less minify task in Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 const gulp = require('gulp'); 
-const cleanCSS = require('gulp-clean-css')
+const cleanCSS = require('gulp-clean-css');
+const less = require('gulp-less');
 
 gulp.task('cssCleanUp', function () {
     return gulp.src('./src/css/index.css')
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('public/css'));
 }); 
+
+gulp.task('lessCleanUp', function() {
+    return gulp.src('./src/less/index.less')
+    .pipe(less())
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('public/css'));
+});
 
 // gulp.task('test', function () {
 //     console.log('Gulp js is running!'); 
