adc83c3e2ad9c09c94f54d8c5f64453216f99cd3
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-08-21 10:01:35 -0500

    Changed the path to index.css to add gulp task to minify CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 const gulp = require('gulp'); 
+const cleanCSS = require('gulp-clean-css')
 
-gulp.task('test', function () {
-    console.log('Gulp js is running!'); 
+gulp.task('cssCleanUp', function () {
+    return gulp.src('./src/css/index.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('public/css'));
 }); 
+
+// gulp.task('test', function () {
+//     console.log('Gulp js is running!'); 
+// }); 
